738d4b31e72760a32f136f66afb913ea58028aee
Artem <artem.ugolkov@ukr.net>
2023-11-07 22:43:28 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,67 @@
+AOS.init();
+
+const burger = document.querySelector('#burger');
+const popup = document.querySelector('#popup');
+const popupSlide = document.querySelector('#popupSlide');
+const sliderItem = document.querySelectorAll('.our_requirements_content_item');
+const sliderPhoto = document.querySelector('.our_requirements_img');
+const body = document.body;
+const casualLogo = document.querySelector('.logo-white');
+const colorLogo = document.querySelector('.logo-color');
+
+burger.addEventListener('click', burgerHandler);
+popup.addEventListener('click', (e) => {
+  burgerHandler(e);
+});
+
+function burgerHandler(e) {
+  if (popup.classList.contains('open')) {
+    popup.classList.add('close');
+    body.classList.remove('noscroll');
+    popupSlide.classList.add('slideout');
+    casualLogo.classList.remove('logo-hide');
+    colorLogo.classList.add('logo-hide');
+    setTimeout(() => {
+      casualLogo.classList.remove('logo-hide');
+      colorLogo.classList.add('logo-hide');
+      // colorLogo.classList.remove('logo-hide');
+      // casualLogo.classList.add('logo-hide');
+    }, 270);
+    setTimeout(() => {
+      popup.classList.remove('close');
+      popup.classList.remove('open');
+      popupSlide.classList.remove('slideout');
+      popupSlide.classList.remove('slidein');
+    }, 270);
+  } else {
+    body.classList.add('noscroll');
+    popup.classList.add('open');
+    popupSlide.classList.add('slidein');
+    setTimeout(() => {
+      //   casualLogo.classList.remove('logo-hide');
+      // colorLogo.classList.add('logo-hide');
+      colorLogo.classList.remove('logo-hide');
+      casualLogo.classList.add('logo-hide');
+    }, 270);
+  }
+  burger.classList.toggle('active');
+}
+
+function scrollToNumbers() {
+  document.querySelector('#numbers').scrollIntoView(true);
+}
+function scrollToSolutions() {
+  document.querySelector('#solutions').scrollIntoView(true);
+}
+function scrollToProcess() {
+  document.querySelector('#process').scrollIntoView(true);
+}
+function scrollToReviews() {
+  document.querySelector('#reviews').scrollIntoView(true);
+}
+function scrollToTeam() {
+  document.querySelector('#team').scrollIntoView(true);
+}
+function scrollToSub() {
+  document.querySelector('#sub').scrollIntoView(true);
+}
